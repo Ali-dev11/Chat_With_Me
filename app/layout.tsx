30c6784e492c097d965e356f16fc32301dbb001b
Ali-dev <alin89328@gmail.com>
2023-11-21 16:31:32 +0500

    feat: created landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
-
 import Header from '@/components/Header'
 
 import './globals.css'
 import {ThemeProvider} from '@/components/ThemeProvider'
 import ClientProviders from '@/components/ClientProviders'
-
-const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -18,7 +14,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<ClientProviders>
 			<html lang='en'>
-				<body className={inter.className}>
+				<body className='flex flex-col min-h-screen'>
 					<ThemeProvider
 						attribute='class'
 						defaultTheme='system'
