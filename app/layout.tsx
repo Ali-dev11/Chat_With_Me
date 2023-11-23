e6e98ecf15a832cb59c56832cf188d408afff840
Ali-dev <alin89328@gmail.com>
2023-11-23 19:02:42 +0500

    feat: added firebase auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import {ThemeProvider} from '@/components/ThemeProvider'
 import ClientProviders from '@/components/ClientProviders'
+import FirebaseAuthProvider from '@/components/FirebaseAuthProvider'
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -15,14 +16,16 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 		<ClientProviders>
 			<html lang='en'>
 				<body className='flex flex-col min-h-screen'>
-					<ThemeProvider
-						attribute='class'
-						defaultTheme='system'
-						enableSystem
-						disableTransitionOnChange>
-						<Header />
-					</ThemeProvider>
-					{children}
+					<FirebaseAuthProvider>
+						<ThemeProvider
+							attribute='class'
+							defaultTheme='system'
+							enableSystem
+							disableTransitionOnChange>
+							<Header />
+							{children}
+						</ThemeProvider>
+					</FirebaseAuthProvider>
 				</body>
 			</html>
 		</ClientProviders>
